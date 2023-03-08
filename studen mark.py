e8cf912f58761465cb7e52758279f267939903ed
Võ Minh Kiệt <125539745+kiethien@users.noreply.github.com>
2023-03-08 20:21:53 +0700

    Add files via upload

---
+++ studen mark.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+def input_student
+    number_of_student = int(input("Enter the number of student: "))
+    info = []
+    print("Enter in order of student class, id, name, date: ")
+    for i in range(number_of_student):
+        student = []
+        for j in range(4):
+            student.append(str(input()))
+        info.append(student)
+
+def search
+
+def input_mark
+
+def print_function
+    print("No | Class | ID | Name | Date")
+    for i in range(number_of_student):
+        for j in range(4):
+            print(info[i][j], end=" ")
+        print()
+
